942af93c7273d887e60674afb81a24ed8469c02d
Marc Portabella Navarro <portabella.marc@gmail.com>
2023-02-14 01:36:10 +0100

    test: fix test (using only json from tests)

--- app/src/androidTest/java/com/mrmpn/videogamedb/ComposableTests.kt
+++ app/src/androidTest/java/com/mrmpn/videogamedb/ComposableTests.kt
@@ -4,10 +4,14 @@ import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
+import com.mrmpn.sharedtestcode.loadFileText
+import com.mrmpn.videogamedb.data.json
+import com.mrmpn.videogamedb.data.models.RawgAPIResponse
+import com.mrmpn.videogamedb.domain.Game
 import com.mrmpn.videogamedb.ui.components.GameCardList
-import com.mrmpn.videogamedb.ui.providers.GamePreviewParameterProvider
 import com.mrmpn.videogamedb.ui.theme.VideogameDBTheme
 import kotlinx.collections.immutable.toImmutableList
+import kotlinx.serialization.decodeFromString
 import org.junit.Rule
 import org.junit.Test
 
@@ -16,14 +20,17 @@ class ComposableTests {
     @get:Rule
     val activityRule = createAndroidComposeRule<ComponentActivity>()
 
+    private val jsonToLoad = loadFileText(this, "/mockResponses/GetGames200.json")
+
     @Test
     fun gameCardListLoadsCorrectly() {
         val tag = activityRule.activity.getString(R.string.tag_item_game_title)
-        val games = GamePreviewParameterProvider().values.toImmutableList()
+
+        val games = json.decodeFromString<RawgAPIResponse<Game>>(jsonToLoad).results
 
         activityRule.setContent {
             VideogameDBTheme {
-                GameCardList(games)
+                GameCardList(games.toImmutableList())
             }
         }
         activityRule.onAllNodesWithTag(tag).assertCountEquals(games.size)
